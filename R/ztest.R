cf2311baa8979f783515aa6c8e5918dafb6f536d
filippogambarota <filippo.gambarota@gmail.com>
2022-12-11 15:15:02 +0100

    removing rounding

--- R/ztest.R
+++ R/ztest.R
@@ -12,7 +12,7 @@
 #' Ztest(0.3, 40, 0, 1)
 Ztest <- function(x.media, n, mi, sigma) {
   z.oss <- (x.media - mi ) / (sigma / sqrt(n))
-  p.value <- round(pnorm(-abs(z.oss), 0, 1) * 2, 4)
+  p.value <- pnorm(-abs(z.oss), 0, 1) * 2
   ris <- data.frame(z.oss, p.value)
   return(ris)
 }
